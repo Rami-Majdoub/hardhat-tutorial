13861203b0c6daca693023b1659154a046864a91
Rami-Majdoub <rami.majdoub1@gmail.com>
2022-03-22 04:51:26 +0100

    add more tests

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,19 +1,26 @@
 // SPDX-License-Identifier:MIT
 pragma solidity 0.8.9;
 
+import "hardhat/console.sol";
+
 contract Token {
   string public name = "Token";
   string public symbol = "TOKEN";
   uint public totalSupply = 1000;
+  address public owner;
 
   mapping (address => uint) balances;
 
   constructor(){
+    owner = msg.sender;
     balances[msg.sender] = totalSupply;
   }
 
   function transfer(address to, uint amount) external {
-    require(balances[msg.sender] - amount >= 0, "transfer: not enough token");
+    console.log("Trying to send %s tokens to %s", amount, to);
+    console.log("Sender balance %d", balances[msg.sender]);
+
+    require(balances[msg.sender] >= amount, "transfer: not enough token");
     balances[msg.sender]-= amount;
     balances[to]+= amount;
   }
